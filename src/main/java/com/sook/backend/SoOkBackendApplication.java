2d832c292b2f7bdd149caa16658665058d4aef4f
GeonHwan Cha <geonhwan.cha@clroot.io>
2022-09-21 14:37:53 +0900

    :tada: init project

---
+++ src/main/java/com/sook/backend/SoOkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sook.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoOkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoOkBackendApplication.class, args);
+	}
+
+}
