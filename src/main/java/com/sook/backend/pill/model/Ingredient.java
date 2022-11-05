a6a21b85e4266416e21509ba83bb12e6237ec53c
GeonHwan Cha <geonhwan.cha@clroot.io>
2022-11-06 01:08:29 +0900

    :construction: add IngredientUnit

--- src/main/java/com/sook/backend/pill/model/Ingredient.java
+++ src/main/java/com/sook/backend/pill/model/Ingredient.java
@@ -1,6 +1,8 @@
 package com.sook.backend.pill.model;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Table;
 
 import com.sook.backend.common.model.BaseModel;
@@ -12,7 +14,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "pill_ingredient")
+@Table(name = "pill_ingredients")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
@@ -20,4 +22,7 @@ import lombok.NoArgsConstructor;
 public class Ingredient extends BaseModel {
 
 	private String name;
+
+	@Enumerated(EnumType.STRING)
+	private IngredientUnit defaultUnit;
 }
