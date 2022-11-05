5f38cc9ac9b6d008fba0084f4032aa3482e05adc
GeonHwan Cha <geonhwan.cha@clroot.io>
2022-11-06 03:12:55 +0900

    :construction: implement SearchRepository

---
+++ src/main/java/com/sook/backend/common/dto/SearchDto.java
@@ -0,0 +1,9 @@
+package com.sook.backend.common.dto;
+
+import java.lang.reflect.Modifier;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.reflect.FieldUtils;
+
+public interface SearchDto {
+}
