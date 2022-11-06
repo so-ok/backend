d84019ded11c8b9025270be425ebda977436987c
GeonHwan Cha <geonhwan.cha@clroot.io>
2022-11-07 02:18:54 +0900

    :construction: implement PillService & PillDto

---
+++ src/main/java/com/sook/backend/pill/dto/PillDto.java
@@ -0,0 +1,68 @@
+package com.sook.backend.pill.dto;
+
+import java.util.List;
+
+import com.sook.backend.attention.model.Attention;
+import com.sook.backend.pill.model.Pill;
+import com.sook.backend.pill.model.PillIngredient;
+
+import lombok.Builder;
+
+public record PillDto(
+		Long id,
+		String name,
+		List<PillIngredientDto> ingredients,
+		List<AttentionPillDto> attentions
+) {
+	@Builder
+	public PillDto {
+	}
+
+	public static PillDto of(Pill entity) {
+		var ingredients = entity.pillIngredients().stream()
+				.map((PillIngredientDto::of))
+				.toList();
+		var attentions = entity.attentions().stream()
+				.map((AttentionPillDto::of))
+				.toList();
+
+		return PillDto.builder()
+				.id(entity.id())
+				.name(entity.name())
+				.ingredients(ingredients)
+				.attentions(attentions)
+				.build();
+	}
+
+	public record AttentionPillDto(
+			String name
+	) {
+		@Builder
+		public AttentionPillDto {
+		}
+
+		public static AttentionPillDto of(Attention entity) {
+			return AttentionPillDto.builder()
+					.name(entity.name())
+					.build();
+		}
+	}
+
+	public record PillIngredientDto(
+			String name,
+			String amount,
+			String unit
+	) {
+		@Builder
+		public PillIngredientDto {
+		}
+
+		public static PillIngredientDto of(PillIngredient entity) {
+			return PillIngredientDto.builder()
+					.name(entity.ingredient().name())
+					.amount(entity.amount().toString())
+					.unit(entity.unit().unit())
+					.build();
+		}
+	}
+}
