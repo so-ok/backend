61c177bc53fd9554377f31988b07f22ae4759b71
0chil <0@chll.it>
2022-11-08 01:38:09 +0900

    :memo: API 문서화

--- src/main/java/com/sook/backend/security/auth/controller/AuthController.java
+++ src/main/java/com/sook/backend/security/auth/controller/AuthController.java
@@ -5,19 +5,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sook.backend.common.annotations.NoApiAuth;
 import com.sook.backend.security.auth.JwtService;
 import com.sook.backend.security.auth.dto.TokenDto;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("auth")
+@Api(tags = "🔑 인증")
 public class AuthController {
 
 	private final JwtService jwtService;
 
+	@ApiOperation(value = "토큰 갱신",notes = "Refresh Token 필요")
 	@GetMapping(path = "renew/{refreshToken}")
+	@NoApiAuth
 	public TokenDto.TokenResponseDto renewToken(@PathVariable String refreshToken) {
 		return jwtService.renew(refreshToken);
 	}
