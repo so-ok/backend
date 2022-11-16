d2b382aaa9570e495613fd1e90e9906ccca26e46
0chil <0@chll.it>
2022-11-16 17:21:40 +0900

    :bug: Jwt 토큰에 이메일이 아닌 userId가 실리는 버그

--- src/main/java/com/sook/backend/security/auth/JwtService.java
+++ src/main/java/com/sook/backend/security/auth/JwtService.java
@@ -1,14 +1,17 @@
 package com.sook.backend.security.auth;
 
+import java.util.List;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 
 import com.sook.backend.security.auth.dto.AuthDto;
 import com.sook.backend.security.auth.dto.TokenDto;
 import com.sook.backend.security.auth.key.JwtKey;
-import com.sook.backend.user.model.User;
+import com.sook.backend.user.dto.UserDto;
 import com.sook.backend.user.service.UserService;
 
 import io.jsonwebtoken.Claims;
@@ -19,47 +22,49 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class JwtService {
 
-	private final UserService userService;
-	private final JwtKey accessKey;
-	private final JwtKey refreshKey;
+    private final UserService userService;
+    private final JwtKey accessKey;
+    private final JwtKey refreshKey;
 
-	public String generateAccessToken(OAuth2User oAuth2User) {
-		Claims claims = buildClaims(oAuth2User);
-		return accessKey.generateTokenWith(claims);
-	}
+    public String generateAccessToken(OAuth2User oAuth2User) {
+        Claims claims = buildClaims(oAuth2User);
+        return accessKey.generateTokenWith(claims);
+    }
 
-	public String generateRefreshToken(OAuth2User oAuth2User) {
-		Claims claims = buildClaims(oAuth2User);
-		return refreshKey.generateTokenWith(claims);
-	}
+    public String generateRefreshToken(OAuth2User oAuth2User) {
+        Claims claims = buildClaims(oAuth2User);
+        return refreshKey.generateTokenWith(claims);
+    }
 
-	public Authentication getAuthentication(String accessToken) {
-		Claims claims = accessKey.parse(accessToken);
-		String email = claims.getSubject();
-		User user = userService.findUser(email);
+    public Authentication getAuthentication(String accessToken) {
+        Claims claims = accessKey.parse(accessToken);
+        String email = claims.getSubject();
+        UserDto userDto = userService.findBy(email);
 
-		AuthDto authDTO = AuthDto.builder()
-			.id(user.id())
-			.email(user.email())
-			.build();
-		return new UsernamePasswordAuthenticationToken(authDTO, "", user.getAuthorities());
-	}
+        AuthDto authDto = AuthDto.builder()
+                .email(userDto.email())
+                .build();
+        List<SimpleGrantedAuthority> authorities = userDto.authorities().stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.name()))
+                .toList();
+        return new UsernamePasswordAuthenticationToken(authDto, "", authorities);
+    }
 
-	public boolean validateToken(String accessToken) {
-		return accessKey.validate(accessToken);
-	}
+    public boolean validateToken(String accessToken) {
+        return accessKey.validate(accessToken);
+    }
 
-	public TokenDto.TokenResponseDto renew(String refreshToken) {
-		Claims claims = refreshKey.parse(refreshToken);
+    public TokenDto.TokenResponseDto renew(String refreshToken) {
+        Claims claims = refreshKey.parse(refreshToken);
 
-		String accessToken = accessKey.generateTokenWith(claims);
-		return new TokenDto.TokenResponseDto(accessToken);
-	}
+        String accessToken = accessKey.generateTokenWith(claims);
+        return new TokenDto.TokenResponseDto(accessToken);
+    }
 
-	private Claims buildClaims(OAuth2User oAuth2User) {
-		String email = oAuth2User.getName();
-		Claims claims = Jwts.claims();
-		claims.setSubject(email);
-		return claims;
-	}
+    private Claims buildClaims(OAuth2User oAuth2User) {
+        String email = oAuth2User.getName();
+        Claims claims = Jwts.claims();
+        claims.setSubject(email);
+        return claims;
+    }
 }
--- src/main/java/com/sook/backend/security/oauth/ThirdPartyOAuth2UserService.java
+++ src/main/java/com/sook/backend/security/oauth/ThirdPartyOAuth2UserService.java
@@ -6,7 +6,6 @@ import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserServ
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,33 +21,33 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ThirdPartyOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
-	private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-	@Transactional
-	@Override
-	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-		OAuth2User oAuth2User = new DefaultOAuth2UserService().loadUser(userRequest);
+    @Transactional
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = new DefaultOAuth2UserService().loadUser(userRequest);
 
-		String registrationId = userRequest.getClientRegistration().getRegistrationId();
-		String userNameAttributeName = userRequest.getClientRegistration()
-			.getProviderDetails()
-			.getUserInfoEndpoint()
-			.getUserNameAttributeName();
-		OAuth2UserAttribute attributes = OAuth2UserAttributeFactory.from(registrationId, oAuth2User.getAttributes(),
-			userNameAttributeName);
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        String userNameAttributeName = userRequest.getClientRegistration()
+                .getProviderDetails()
+                .getUserInfoEndpoint()
+                .getUserNameAttributeName();
+        OAuth2UserAttribute attributes = OAuth2UserAttributeFactory.from(registrationId, oAuth2User.getAttributes(),
+                userNameAttributeName);
 
-		User savedUser = saveUser(attributes);
-		return new DefaultOAuth2User(savedUser.getAuthorities(), attributes.getAttributes(), userNameAttributeName);
-	}
+        User savedUser = saveUser(attributes);
+        return new SookOAuth2User(savedUser.getAuthorities(), attributes);
+    }
 
-	private User saveUser(OAuth2UserAttribute attributes) {
-		Optional<User> userOptional = userRepository.findByEmail(attributes.email());
-		if (userOptional.isPresent()) {
-			User user = userOptional.get();
-			user.updateImage(attributes.profileImage());
-			return user;
-		}
+    private User saveUser(OAuth2UserAttribute attributes) {
+        Optional<User> userOptional = userRepository.findByEmail(attributes.email());
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            user.updateImage(attributes.profileImage());
+            return user;
+        }
 
-		return userRepository.save(attributes.toEntity());
-	}
+        return userRepository.save(attributes.toEntity());
+    }
 }
