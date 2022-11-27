77a075bdae991ad3185da13238731cb3e87d463e
0chil <0@chll.it>
2022-11-27 20:34:28 +0900

    :wrench: remove wildcard for SecurityConfig

--- src/main/java/com/sook/backend/common/config/SecurityConfig.java
+++ src/main/java/com/sook/backend/common/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
 
 				.authorizeRequests()
 				.antMatchers("/docs", "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs").permitAll()
-				.antMatchers("/auth/renew/**").permitAll()
+				.antMatchers("/auth/renew").permitAll()
 				.antMatchers("/api/pill/**").permitAll()
 				.antMatchers("/api/user/{email}").permitAll()
 				.requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
