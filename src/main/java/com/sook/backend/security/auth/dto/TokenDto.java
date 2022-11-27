a1f95ed9790d40af34419fda2d7f82a429490c0c
0chil <0@chll.it>
2022-11-27 20:29:56 +0900

    :recycle: AccessToken, RefreshToken as nested record

--- src/main/java/com/sook/backend/security/auth/dto/TokenDto.java
+++ src/main/java/com/sook/backend/security/auth/dto/TokenDto.java
@@ -2,15 +2,30 @@ package com.sook.backend.security.auth.dto;
 
 import lombok.Builder;
 
-public record TokenDto(
-        String token
-) {
-    @Builder
-    public TokenDto {
+public class TokenDto {
+    public record AccessToken(
+            String accessToken
+    ) {
+        @Builder
+        public AccessToken {
+        }
+
+        @Override
+        public String toString() {
+            return "token";
+        }
     }
 
-    @Override
-    public String toString() {
-        return "token";
+    public record RefreshToken(
+            String refreshToken
+    ) {
+        @Builder
+        public RefreshToken {
+        }
+
+        @Override
+        public String toString() {
+            return "token";
+        }
     }
 }
