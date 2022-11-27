e9194b55443dbd64fb481e6cfa398b20238a03d8
0chil <0@chll.it>
2022-11-27 20:23:17 +0900

    :recycle: AuthDto 표현 수정

--- src/main/java/com/sook/backend/security/auth/dto/AuthDto.java
+++ src/main/java/com/sook/backend/security/auth/dto/AuthDto.java
@@ -1,16 +1,4 @@
 package com.sook.backend.security.auth.dto;
 
-import static lombok.AccessLevel.*;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor(access = PRIVATE)
-@Builder
-public class AuthDto {
-
-    private final Long id;
-    private final String email;
+public record AuthDto(String email) {
 }
