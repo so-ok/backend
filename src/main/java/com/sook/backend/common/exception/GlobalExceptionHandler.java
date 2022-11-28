56564dd949cfef5ecf3e25c5f7897dfc68549948
GeonHwan Cha <geonhwan.cha@clroot.io>
2022-11-28 13:37:29 +0900

    :sparkles: Print Log on GlobalExceptionHandler

--- src/main/java/com/sook/backend/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sook/backend/common/exception/GlobalExceptionHandler.java
@@ -7,11 +7,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.sook.backend.common.dto.ExceptionDto;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ExceptionDto> handleException(final Exception exception) {
+        log.error("INTERNAL_SERVER_ERROR", exception);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ExceptionDto.of(exception));
@@ -19,6 +23,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ApiException.class)
     protected ResponseEntity<ExceptionDto> handleApiException(final ApiException exception) {
+        log.error("API_EXCEPTION", exception);
         return ResponseEntity
                 .status(exception.getStatus())
                 .body(ExceptionDto.of(exception));
