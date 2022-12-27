875b932a5ebd13d5a3d32b719ec8aee994f00349
0chil <0@chll.it>
2022-12-27 20:48:34 +0900

    :construction: trigger build and test

--- src/main/java/com/sook/backend/common/exception/ApiException.java
+++ src/main/java/com/sook/backend/common/exception/ApiException.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 public abstract class ApiException extends ResponseStatusException {
-
     private final String message;
 
     public ApiException(final HttpStatus status, final String message) {
