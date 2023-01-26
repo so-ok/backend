ff45b5434757fdaab4f153a948ed1f34fc9fa387
0chil <0@chll.it>
2023-01-26 13:05:22 +0900

    :recycle: removed roles from UserDto

--- src/main/java/com/sook/backend/user/dto/UserDto.java
+++ src/main/java/com/sook/backend/user/dto/UserDto.java
@@ -1,9 +1,5 @@
 package com.sook.backend.user.dto;
 
-import java.util.List;
-
-import org.springframework.security.core.GrantedAuthority;
-
 import com.sook.backend.user.model.User;
 
 import lombok.Builder;
@@ -11,8 +7,7 @@ import lombok.Builder;
 public record UserDto(
         String username,
         String email,
-        String image,
-        List<AuthorityDto> authorities
+        String image
 ) {
 
     @Builder
@@ -20,29 +15,11 @@ public record UserDto(
     }
 
     public static UserDto of(User entity) {
-        List<AuthorityDto> authorities = entity.getAuthorities().stream()
-                .map(AuthorityDto::of)
-                .toList();
 
         return UserDto.builder()
                 .username(entity.username())
                 .email(entity.email())
                 .image(entity.image())
-                .authorities(authorities)
                 .build();
-    }
-
-    public record AuthorityDto(
-            String name
-    ) {
-        @Builder
-        public AuthorityDto {
-        }
-
-        public static AuthorityDto of(GrantedAuthority authority) {
-            return AuthorityDto.builder()
-                    .name(authority.getAuthority())
-                    .build();
-        }
     }
 }
