0fc9ffe05f795a49c73bc16116faf3322b0d93b9
0chil <0@chll.it>
2023-01-26 13:15:05 +0900

    :recycle: removed unnecessary method and dependency

--- src/main/java/com/sook/backend/user/model/User.java
+++ src/main/java/com/sook/backend/user/model/User.java
@@ -2,10 +2,8 @@ package com.sook.backend.user.model;
 
 import java.io.Serial;
 import java.time.LocalDateTime;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
@@ -17,9 +15,6 @@ import javax.persistence.FetchType;
 import javax.persistence.Index;
 import javax.persistence.Table;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
 import com.sook.backend.common.model.BaseModel;
 
 import lombok.AccessLevel;
@@ -30,55 +25,47 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "sook_user", indexes = {
-		@Index(name = "sook_user_username_index", columnList = "username"),
-		@Index(name = "sook_user_user_id_index", columnList = "user_id, oauth_provider", unique = true),
+        @Index(name = "sook_user_username_index", columnList = "username"),
+        @Index(name = "sook_user_user_id_index", columnList = "user_id, oauth_provider", unique = true),
 })
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Builder
 @AllArgsConstructor
 public class User extends BaseModel {
-	@Serial
-	private static final long serialVersionUID = 1L;
+    @Serial
+    private static final long serialVersionUID = 1L;
 
-	@Column(name = "user_id", unique = true, nullable = false)
-	private String userId;
+    @Column(name = "user_id", unique = true, nullable = false)
+    private String userId;
 
-	@Column(name = "username", nullable = false)
-	private String username;
+    @Column(name = "username", nullable = false)
+    private String username;
 
-	@Column(name = "password")
-	private String password;
+    @Column(name = "password")
+    private String password;
 
-	@Column(name = "email", unique = true, nullable = false)
-	private String email;
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
 
-	@Column(name = "image")
-	private String image;
+    @Column(name = "image")
+    private String image;
 
-	@Column(name = "oauth_provider", nullable = false)
-	private String oauthProvider;
+    @Column(name = "oauth_provider", nullable = false)
+    private String oauthProvider;
 
-	@Builder.Default
-	private boolean enabled = true;
+    @Builder.Default
+    private boolean enabled = true;
 
-	@ElementCollection(fetch = FetchType.EAGER)
-	@CollectionTable(name = "sook_user_roles")
-	@Enumerated(EnumType.STRING)
-	@Builder.Default
-	private Set<Role> roles = new HashSet<>();
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "sook_user_roles")
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private Set<Role> roles = new HashSet<>();
 
-	private LocalDateTime lastLogin;
+    private LocalDateTime lastLogin;
 
-	public void updateImage(String imageUrl) {
-		image = imageUrl;
-	}
-
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return roles
-			.stream()
-			.map(Role::key)
-			.map(SimpleGrantedAuthority::new)
-			.collect(Collectors.toList());
-	}
+    public void updateImage(String imageUrl) {
+        this.image = imageUrl;
+    }
 }
